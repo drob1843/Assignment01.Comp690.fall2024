a70ac7c0080e174305bb312c7a7e2807a3a017ee
drob1843 <94759314+drob1843@users.noreply.github.com>
2024-09-22 16:01:48 -0700

    Half way there

--- js/script.js
+++ js/script.js
@@ -1,15 +1,96 @@
-// STEP 1
+"use strict";
+
+
+// STEP 1 
+
+let someMonth;
+function theMonth(){};
+let currentMonth;
+let summerMonth;
+let myLibraryFunction;
+
 // STEP 2
+
+const count = 21;
+let nickName = "El Chingon";
+let isValid = false;
+const nombre = null;
+
 // STEP 3
+
+let cinco = 4 + 1;
+const pName = "Juan", uName = "Do";
+
+
 // STEP 4
+
+let firstName;
+let lastName;
+let strAddress;
+let strCity;
+let State;
+let intZipCode;
+let intAge;
+let strReferralSource;
+let blnResponse;
+
 // STEP 5
+
+let fName ="Diego";
+let zipCode = 85749;
+const response = true;
+
 // STEP 6
+
+let fruit = "apple" , ammount = 10
+
+console.log(`I ate ${ammount} ${fruit}\'s`);
+
 // STEP 7
+
+let jumpA = true, whoA = "Mary";
+let jumpB = false, whoB = 10;
+
+let resultA = whoA + " : " + jumpA;
+let resultB = whoB + " : " + jumpB;
+
+console.log(resultA);
+console.log(resultB);
+
 // STEP 8
+
+let someString = 'Who once said, \"Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former.\"';
+console.log(someString);
+
 // STEP 9
+
+let unDefinded;
+let test = null;
+
+let testResult = test + unDefinded;
+
+console.info(testResult);
+
 // STEP 10
+
+console.info(typeof "Hat");
+console.info(typeof 33);
+console.info(typeof false);
+console.info(typeof window);
+console.info(typeof blah);
+
 // STEP 11
+
+alert("Hello " + fName +" , welcome to the JavaScript class!");
+
 // STEP 12
 // STEP 13
 // STEP 14
 // STEP 15
+// STEP 16
+// STEP 17
+// STEP 18
+// STEP 19
+// STEP 20
+// STEP 21
+// STEP 22
